48a92327986bcc8134a3cc058dd904679e26eed2
Tsedi28 <tsedi22@gmail.com>
2023-02-15 21:10:59 -0500

    add books route

---
+++ routes/api/books.js
@@ -0,0 +1,29 @@
+const { Book } = require("../../models");
+const booksController = require("../../controllers/booksController");
+const router = require("express").Router();
+
+// Matches with "/api/books"
+router.route("/").get(booksController.findAll).post(booksController.create);
+
+// Matches with "api/books/:id"
+router
+  .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
+  .delete(booksController.remove)
+ 
+router.put("/renting/:id", async (req, res) => {
+  try {
+    console.log(req.params.id);
+    let book = await Book.findByIdAndUpdate(req.params.id, {
+      currentRenter: req.body.currentRenter,
+    });
+    console.log(book);
+    res.json(book);
+  } catch (ex) {
+    res.json(ex);
+  }
+});
+
+
+module.exports = router;
