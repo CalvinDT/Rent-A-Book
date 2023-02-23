b18ce574b23c5807bb7670a2c281f4d93eca6f97
CalvinDT <ct223344@gmail.com>
2023-02-23 07:57:28 -0500

    Update connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/rent-a-book', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   }
